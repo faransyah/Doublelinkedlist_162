60a9ec0d6229224c29df3a1751d684ce35a7bdaa
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-05-26 11:08:27 +0700

    menambahkan string nm dan int nim

--- Doublelinkedlist_162.cpp
+++ Doublelinkedlist_162.cpp
@@ -30,5 +30,11 @@ DoubleLinkedList::DoubleLinkedList() {
 }
 
 void DoubleLinkedList::addnode() {
-
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	
 }

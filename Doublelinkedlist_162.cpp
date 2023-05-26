90e4bedfaee6dd217e1c897d36b0ea0488717e7f
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-05-26 11:31:24 +0700

    bool doublelinkedlist

--- Doublelinkedlist_162.cpp
+++ Doublelinkedlist_162.cpp
@@ -70,6 +70,11 @@ void DoubleLinkedList::addnode() {
 
 	newNode->next = current->next; //step 4
 	newNode->prev = current; //step 5
+	if (current->next != NULL)
+		current->next->prev = newNode; // step 6
+	current->next = newNode; //step 7
+}
 
+bool DOubleLinkedList::search(int rollNo, Node** previous, Node** current) {
 
 }
